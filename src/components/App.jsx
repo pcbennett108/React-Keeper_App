1301db9a0b8fe071757c9f3512016beb355f9bbe
pcbennett108 <pcbennett42@gmail.com>
2022-05-13 21:00:37 -0600

    add notes file map function to that file.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      <p>
+        {notes.map((noteObj) => (
+          <Note
+            key={noteObj.key}
+            title={noteObj.title}
+            content={noteObj.content}
+          />
+        ))}
+      </p>
       <Footer />
     </div>
   );
